5ea0ac1e5501ba284e4e380600303ede793c9695
jnorthrup <northrup.james@gmail.com>
2014-04-05 10:58:44 -0700

    configs.

--- kvdb/src/main/java/KouchConfig.java
+++ kvdb/src/main/java/KouchConfig.java
@@ -1,5 +1,5 @@
- import com.google.gson.Gson;
- import javolution.util.FastMap;
+import com.google.gson.Gson;
+import javolution.util.FastMap;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,31 +9,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
-
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 
 public class KouchConfig {
-    static Map bootstrap=new FastMap<>();
-static{
-    Path kouch_ini = Paths.get(getVar("KOUCH_INI", "kouch.ini"));
-    try {
-        try (FileInputStream in = new FileInputStream(kouch_ini.toFile())) {
-            try (InputStreamReader fileInputStream = new InputStreamReader(in)) {
-                Gson gson = new Gson();
-                Map map = gson.fromJson(fileInputStream, Map.class);
-            }
+    static Map<String, ?> bootstrap = new FastMap<>();
+
+    static {
+        Path kouch_ini = Paths.get(getVar("KOUCH_INI_JSON", "kouch.ini.json"));
+        try (FileInputStream in = new FileInputStream(kouch_ini.toFile()); InputStreamReader fileInputStream = new InputStreamReader(in)) {
+            Gson gson = new Gson();
+            bootstrap.putAll(gson.<Map>fromJson(fileInputStream, Map.class));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        ((FastMap)bootstrap).putAll(bootstrap);
-    } catch (IOException e) {
-        e.printStackTrace();
     }
-}
+
     public static String getVar(String kouch_var, String... defaultVal) {
         String javapropname =
                 "kouch." + kouch_var.toLowerCase().replaceAll("^kouch_?", "").replace('_', '.');
         String kouchenv = System.getenv(kouch_var);
         String var = null == kouchenv ? System.getProperty(javapropname) : kouchenv;
-        var=null==var?bootstrap.get(javapropname).toString():var;
+        var = null == var ? bootstrap.get(javapropname).toString() : var;
         var = null == var && defaultVal.length > 0 ? defaultVal[0] : var;
         if (null != var) {
             System.setProperty(javapropname, var);
