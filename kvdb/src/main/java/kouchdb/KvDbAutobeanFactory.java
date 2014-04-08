3514289f8d5cb94263cadff6a8641cc19c2648e0
jnorthrup <northrup.james@gmail.com>
2014-04-07 20:39:33 -0700

    protostuff generation

--- kvdb/src/main/java/kouchdb/KvDbAutobeanFactory.java
+++ kvdb/src/main/java/kouchdb/KvDbAutobeanFactory.java
@@ -2,14 +2,16 @@ package kouchdb;
 
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanFactory;
-import kouchdb.command.Commands;
+import kouchdb.command.BatchFetch;
+import kouchdb.command.Fetchdoc;
+import kouchdb.command.Replication;
 
 public interface KvDbAutobeanFactory extends AutoBeanFactory {
 AutoBean<ViewResults.Results> viewResults();
 AutoBean<ViewResults.Results> viewResults(ViewResults.Results x);
-AutoBean<Commands.BatchFetch.AllDocOptions> allDocs();
-AutoBean<Commands.BatchFetch.AllDocOptions> allDocs(Commands.BatchFetch.AllDocOptions a);
-AutoBean<Commands.Sync.ReplicateOptions> replicateOptions();
-AutoBean<Commands.Fetchdoc.FetchOptions> fetchoptions();
-AutoBean<Commands.Fetchdoc.FetchOptions> fetchoptions(Commands.Fetchdoc.FetchOptions options);
+AutoBean<BatchFetch.AllDocOptions> allDocs();
+AutoBean<BatchFetch.AllDocOptions> allDocs(BatchFetch.AllDocOptions a);
+AutoBean<Replication.ReplicateOptions> replicateOptions();
+AutoBean<Fetchdoc.FetchOptions> fetchoptions();
+AutoBean<Fetchdoc.FetchOptions> fetchoptions(Fetchdoc.FetchOptions options);
 }
