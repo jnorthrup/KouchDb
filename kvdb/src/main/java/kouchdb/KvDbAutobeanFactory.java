8ffad797161285d83db5f92f47d808e7d7e5905b
jnorthrup <northrup.james@gmail.com>
2014-04-16 11:53:34 -0700

    put the runtime interface assumptions on hold to examine generation approaches

--- kvdb/src/main/java/kouchdb/KvDbAutobeanFactory.java
+++ kvdb/src/main/java/kouchdb/KvDbAutobeanFactory.java
@@ -1,17 +1,13 @@
 package kouchdb;
 
-import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanFactory;
-import kouchdb.command.FetchBatch;
-import kouchdb.command.Fetchdoc;
-import kouchdb.command.Replication;
 
 public interface KvDbAutobeanFactory extends AutoBeanFactory {
-AutoBean<ViewResults.Results> viewResults();
-AutoBean<ViewResults.Results> viewResults(ViewResults.Results x);
-AutoBean<FetchBatch.AllDocOptions> allDocs();
-AutoBean<FetchBatch.AllDocOptions> allDocs(FetchBatch.AllDocOptions a);
-AutoBean<Replication.ReplicateOptions> replicateOptions();
-AutoBean<Fetchdoc.FetchOptions> fetchoptions();
-AutoBean<Fetchdoc.FetchOptions> fetchoptions(Fetchdoc.FetchOptions options);
+//AutoBean<ViewResults.Results> viewResults();
+//AutoBean<ViewResults.Results> viewResults(ViewResults.Results x);
+//AutoBean<FetchBatch.AllDocOptions> allDocs();
+//AutoBean<FetchBatch.AllDocOptions> allDocs(FetchBatch.AllDocOptions a);
+//AutoBean<Replication.ReplicateOptions> replicateOptions();
+//AutoBean<Fetchdoc.FetchOptions> fetchoptions();
+//AutoBean<Fetchdoc.FetchOptions> fetchoptions(Fetchdoc.FetchOptions options);
 }
