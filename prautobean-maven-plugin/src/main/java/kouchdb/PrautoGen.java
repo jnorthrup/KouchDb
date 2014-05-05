1afdf1f5f8bb7c874f6230661d513265ca76bdaa
jnorthrup <northrup.james@gmail.com>
2014-05-05 16:23:21 -0700

    viable maven plugin to convert protogen parsed proto into a simpler autobean candidate.

--- prautobean-maven-plugin/src/main/java/kouchdb/PrautoGen.java
+++ prautobean-maven-plugin/src/main/java/kouchdb/PrautoGen.java
@@ -16,15 +16,16 @@ package kouchdb;
  * limitations under the License.
  */
 
-import com.dyuproject.protostuff.parser.EnumGroup;
 import com.dyuproject.protostuff.parser.*;
- import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.*;
@@ -34,74 +35,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Goal which touches a timestamp file.
- *
-  */
-@Mojo( name = "prautobean-maven-plugin"/*, defaultPhase = LifecyclePhase.PROCESS_SOURCES*/ )
+ */
+@Mojo(name = "prautobean", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class PrautoGen
-    extends AbstractMojo
-{
-
-    @Parameter(defaultValue = "${project.basedir}/src/main/proto")
-    public String sourceDirectory;
-    @Parameter(defaultValue = "${project.build.outputDirectory}/generated-sources")
-    public String outputDirectory;
-//
-//    @Override
-//    public void execute() throws MojoExecutionException, MojoFailureException {
-//
-//    }
-    @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-
-        try {
-            String[] args = new String[]{sourceDirectory, outputDirectory};
-
-            Path path = Paths.get(args.length > 0 ? args[0] : "src/main/proto/");
-
-            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-                        throws IOException {
-                    boolean regularFile = attrs.isRegularFile();
-                    if (regularFile) {
-                        try {
-
-                            Proto target = new Proto();
-                            ProtoUtil.loadFrom(Files.newBufferedReader(file), target);
-                            String javaPackageName = target.getJavaPackageName();
-                            outdir = Paths.get((args.length > 1 ? args[1] : "target/generated-sources") + '/' + javaPackageName.replace('.', '/'));
-
-                            Path directories = Files.createDirectories(PrautoGen.outdir);
-                            System.out.println("writing to " + directories.toUri());
-
-                            target.getMessages().forEach(PrautoGen::descend);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        PrautoGen.enums.forEach(PrautoGen::printEnum);
-                        PrautoGen.messages.forEach(PrautoGen::printMessage);
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-
-
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        extends AbstractMojo {
+ 
     static Map<String, StripeLeveler> messages = new LinkedHashMap<>();
-    private static Map<String, EnumGroup> enums = new LinkedHashMap<>();
-    private static Path outdir;
+    static Map<String, EnumGroup> enums = new LinkedHashMap<>();
+    static String outdir;
+    @Parameter(defaultValue = "${project.basedir}/src/main/proto")
+    public File sourceDirectory;
+    @Parameter(defaultValue = "${project.build.directory}/generated-sources")
+    public File outputDirectory;
 
-    private static void printMessage(String k, StripeLeveler v) {
-
+    static void printMessage(String k, StripeLeveler v) {
         String javaPackageName = v.message.getProto().getJavaPackageName();
         String[] split = k.split("\\.");
         String cname = split[split.length - 1];
-        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(outdir.toString(), cname+".java"));PrintWriter pw=new PrintWriter(bufferedWriter)) {
-            pw.println("package " + javaPackageName +
-                    ";\n\n@ProtoOrigin(" + '"' + v.message.getFullName() + '"' + ")\ninterface " + cname + "{");
+        ArrayList<String> strings = new ArrayList<>();
+        Collections.addAll(strings, javaPackageName.split("\\."));
+        strings.add(cname+".java");
+        Path path = Paths.get(outdir, (String[]) strings.toArray(new String[strings.size()]));
+        //System.err.println("writing message: " + path);
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path,StandardOpenOption.CREATE,StandardOpenOption.TRUNCATE_EXISTING,StandardOpenOption.WRITE);
+             PrintWriter pw = new PrintWriter(bufferedWriter)
+        ) {
+            pw.println("package " + javaPackageName + ";import kouchdb.ann.*;\n\n\n@ProtoOrigin(" + '"' + v.message.getFullName() + '"' + ")\ninterface " + cname + "{");
             Map<String, List<Field>> f = v.stripes;
             AtomicInteger bits = new AtomicInteger(0);
             //package up the primitives first
@@ -112,34 +71,36 @@ public class PrautoGen
                 if (null != fields)
                     print(bits, o, fields, ((PrintWriter) pw));
             });
-            f.entrySet().stream().filter(o -> !StripeLeveler.inFirst.contains(o.getKey())).forEachOrdered(o -> print(bits,o.getKey(),o.getValue(), ((PrintWriter) pw)));
+            f.entrySet().stream().filter(o -> !StripeLeveler.inFirst.contains(o.getKey())).forEachOrdered(o -> print(bits, o.getKey(), o.getValue(), ((PrintWriter) pw)));
 
-            pw.println("}");  } catch (IOException e) {
+            pw.println("}");
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    private static void print(AtomicInteger bits, String type, List<Field> fields, PrintWriter printWriter) {
-
+    static void print(AtomicInteger bits, String type, List<Field> fields, PrintWriter printWriter) {
         fields.forEach(field -> {
             String capped = getCaps(field.getName());
             boolean repeated = field.isRepeated();
             boolean optional = field.isOptional();
-            if (!repeated)
-                printWriter.println((optional ? "\n\t@Optional(" + bits.incrementAndGet() + ")" : "") + "\n\t@ProtoNumber(" + field.getNumber() + ")\n\t" + (type) + "\tget" + capped + "();\n");
-            else
-                printWriter.println((optional ? "\n\t@Optional(" + bits.incrementAndGet() + ")" : "") + "\n\t@ProtoNumber(" + field.getNumber() + ")\n\tList<" + (type) + ">\tget" + capped + "();\n");
-
+            printWriter.println((optional ? "\n\t@Optional(" + bits.incrementAndGet() + ")" : "") + "\n\t@ProtoNumber(" + field.getNumber() + ")\n\t" +
+                    (repeated ? ("java.util.List<" + (type) + ">") : (type)) + "\tget" + capped + "();\n");
         });
     }
 
-    private static void printEnum(String k, EnumGroup v) {
-
+    static void printEnum(String k, EnumGroup v) {
         String javaPackageName = v.getProto().getJavaPackageName();
-        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(outdir.toString(), k+".java")); PrintWriter pw= new PrintWriter(bufferedWriter)) {
-            pw.println("package " + javaPackageName +
-                    ";\nenum " + k + "{");
+        String[] split = k.split("\\.");
+        String cname = split[split.length - 1];
+        ArrayList<String> strings = new ArrayList<>();
+        Collections.addAll(strings, javaPackageName.split("\\."));
+        strings.add(cname+".java");
+        Path path = Paths.get(outdir, (String[]) strings.toArray(new String[strings.size()]));
+        //System.err.println("writing enum: " + path);
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path); PrintWriter pw = new PrintWriter(bufferedWriter)) {
+
+            pw.println("package " + javaPackageName + ";import kouchdb.ann.*;\n\n\nenum " + k + "{");
             StringJoiner stringJoiner = new StringJoiner(",");
             v.getValues().stream().map(EnumGroup.Value::getName).forEachOrdered(stringJoiner::add);
             pw.println(stringJoiner.toString());
@@ -147,14 +108,15 @@ public class PrautoGen
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    private static String getCaps(String name) {
+    static String getCaps(String name) {
         return Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
 
-    private static void descend(Message Message) {
+    static void descend(Message Message) {
+
+        //System.err.println("descend: " + Message.getFullName());
         LinkedHashMap<String, Message> nestedMessageMap = Message.getNestedMessageMap();
         for (Map.Entry<String, Message> stringMessageEntry : nestedMessageMap.entrySet()) {
             descend(stringMessageEntry.getValue());
@@ -163,11 +125,12 @@ public class PrautoGen
     }
 
     static void writeGen(Message message) {
+
         StripeLeveler x = new StripeLeveler(message);
         messages.put(message.getFullName(), x);
         List<Field<?>> fields = message.getFields();
         for (Field<?> field : fields) {
-            String javaType1 = field.getJavaType();
+            String javaType1 = field.getJavaType().replace("ByteString","byte[]");
             x.addField(field, javaType1);
         }
         Collection<EnumGroup> nestedEnumGroups = message.getNestedEnumGroups();
@@ -175,7 +138,49 @@ public class PrautoGen
             String name = o.getName();
             enums.put(name, o);
         });
+    }
 
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        try {
+/*            String[] args = new String[]{sourceDirectory, outputDirectory};
+
+            Path path = Paths.get(args.length > 0 ? args[0] : "src/main/proto/");*/
+            outdir = outputDirectory.getAbsolutePath();
+            //System.err.println("executing out: " + outdir + " in: " + sourceDirectory.getAbsolutePath());
+
+            Files.walkFileTree(Paths.get(sourceDirectory.getCanonicalPath()), new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    boolean regularFile = attrs.isRegularFile();
+                    if (regularFile) {
+                        try {
+
+                            Proto target = new Proto();
+                            ProtoUtil.loadFrom(Files.newBufferedReader(file), target);
+                            String javaPackageName = target.getJavaPackageName();
+
+                            Path directories = Files.createDirectories(Paths.get(outdir, javaPackageName.split("\\.")));
+                            //System.err.println("writing to " + directories.toUri());
+
+                            target.getMessages().forEach(PrautoGen::descend);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        enums.forEach(PrautoGen::printEnum);
+                        messages.forEach(PrautoGen::printMessage);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     static class StripeLeveler {
@@ -188,13 +193,15 @@ public class PrautoGen
                 inFirst.add(aClass.getSimpleName());
             }
         }
+
         public Map<String, List<Field>> stripes = new LinkedHashMap<>();
-        private Message message;
+        Message message;
 
         public StripeLeveler(Message message) {
             this.message = message;
         }
-        private void addField(Field<?> field, String javaType1) {
+
+        void addField(Field<?> field, String javaType1) {
             Map<String, List<Field>> f = this.stripes;
             List<Field> fieldList = f.get(javaType1);
             if (fieldList == null) {
@@ -204,5 +211,4 @@ public class PrautoGen
             fieldList.add(field);
         }
     }
-
 }
